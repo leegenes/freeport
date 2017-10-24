1a5322d60e6a376c871b89b78cbdc183f91e1ba4
Lee Haugen <haugenlee@leegenes.local>
2017-10-24 10:33:53 -0700

    adjust window size on startup

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url');
 let window
 
 function create() {
-  window = new BrowserWindow({width: 800, height: 600})
+  window = new BrowserWindow({width: 900, height: 600})
 
   window.loadURL(url.format({
     pathname: path.join(__dirname, '/dist/index.html'),
